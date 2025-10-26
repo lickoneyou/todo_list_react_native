89490720e837c283399944ac07d494b9cff356ef
Kiryl Khomchanka <dickdurex265@gmail.com>
2025-10-26 06:01:21 +0300

    updated app

--- todo_list/app/index.tsx
+++ todo_list/app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { COLORS }                 from '@/constants/colors';
+import { StatusBar }              from 'expo-status-bar';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <StatusBar style={'light'} />
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:            1,
+    justifyContent:  'center',
+    alignItems:      'center',
+    backgroundColor: COLORS.PRIMARY_BLACK,
+  },
+  text:      {
+    color: COLORS.PRIMARY_WHITE,
+  },
+});
---
+++ todo_list/constants/colors.ts
@@ -0,0 +1,4 @@
+export const COLORS = {
+  PRIMARY_WHITE: '#ffffff',
+  PRIMARY_BLACK: '#000000',
+};
